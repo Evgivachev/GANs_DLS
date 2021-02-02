0e640bfb8c3d9889807ff682efb9bb54c429a5d2
Евгений Ивачёв <evgivachev@gmail.com>
2021-02-02 15:17:25 +0300

    1

---
+++ NLayerDiscriminator.py
@@ -0,0 +1,26 @@
+from torch import nn
+from ops import conv_norm_lrelu, get_norm_layer, init_network
+
+
+class NLayerDiscriminator(nn.Module):
+    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d, use_bias=False):
+        super(NLayerDiscriminator, self).__init__()
+        dis_model = [nn.Conv2d(input_nc, ndf, kernel_size=4, stride=2, padding=1),
+                     nn.LeakyReLU(0.2, True)]
+        nf_mult = 1
+        nf_mult_prev = 1
+        for n in range(1, n_layers):
+            nf_mult_prev = nf_mult
+            nf_mult = min(2**n, 8)
+            dis_model += [conv_norm_lrelu(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=4, stride=2,
+                                               norm_layer= norm_layer, padding=1, bias=use_bias)]
+        nf_mult_prev = nf_mult
+        nf_mult = min(2**n_layers, 8)
+        dis_model += [conv_norm_lrelu(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=4, stride=1,
+                                               norm_layer= norm_layer, padding=1, bias=use_bias)]
+        dis_model += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=4, stride=1, padding=1)]
+
+        self.dis_model = nn.Sequential(*dis_model)
+
+    def forward(self, input):
+        return self.dis_model(input)
---
+++ ResnetGenerator.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+import functools
+
+
+class ResnetGenerator(nn.Module):
+    def __init__(self, input_nc=3, output_nc=3, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=True, num_blocks=6):
+        super(ResnetGenerator, self).__init__()
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        res_model = [nn.ReflectionPad2d(3),
+                     conv_norm_relu(input_nc, ngf * 1, 7, norm_layer=norm_layer, bias=use_bias),
+                     conv_norm_relu(ngf * 1, ngf * 2, 3, 2, 1, norm_layer=norm_layer, bias=use_bias),
+                     conv_norm_relu(ngf * 2, ngf * 4, 3, 2, 1, norm_layer=norm_layer, bias=use_bias)]
+
+        for i in range(num_blocks):
+            res_model += [ResidualBlock(ngf * 4, norm_layer, use_dropout, use_bias)]
+
+        res_model += [dconv_norm_relu(ngf * 4, ngf * 2, 3, 2, 1, 1, norm_layer=norm_layer, bias=use_bias),
+                      dconv_norm_relu(ngf * 2, ngf * 1, 3, 2, 1, 1, norm_layer=norm_layer, bias=use_bias),
+                      nn.ReflectionPad2d(3),
+                      nn.Conv2d(ngf, output_nc, 7),
+                      nn.Tanh()]
+        self.res_model = nn.Sequential(*res_model)
+
+    def forward(self, x):
+        return self.res_model(x)
+
+
+def conv_norm_relu(in_dim, out_dim, kernel_size, stride=1, padding=0,
+                   norm_layer=nn.BatchNorm2d, bias=False):
+    return nn.Sequential(
+        nn.Conv2d(in_dim, out_dim, kernel_size, stride, padding, bias=bias),
+        norm_layer(out_dim), nn.ReLU(True))
+
+
+def dconv_norm_relu(in_dim, out_dim, kernel_size, stride=1, padding=0, output_padding=0,
+                    norm_layer=nn.BatchNorm2d, bias=False):
+    return nn.Sequential(
+        nn.ConvTranspose2d(in_dim, out_dim, kernel_size, stride,
+                           padding, output_padding, bias=bias),
+        norm_layer(out_dim), nn.ReLU(True))
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, dim, norm_layer, use_dropout, use_bias):
+        super(ResidualBlock, self).__init__()
+        res_block = [nn.ReflectionPad2d(1),
+                     conv_norm_relu(dim, dim, kernel_size=3,
+                                    norm_layer=norm_layer, bias=use_bias)]
+        if use_dropout:
+            res_block += [nn.Dropout(0.5)]
+        res_block += [nn.ReflectionPad2d(1),
+                      nn.Conv2d(dim, dim, kernel_size=3, padding=0, bias=use_bias),
+                      norm_layer(dim)]
+
+        self.res_block = nn.Sequential(*res_block)
+
+    def forward(self, x):
+        return x + self.res_block(x)
